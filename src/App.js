c8b86268d7fd1c39b8ec5c35ee948ac657859442
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2022-06-20 23:17:06 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+  <NavBar />
+  );
+  
+}
+<script src='<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+'></script>
+export default App;
